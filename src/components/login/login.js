e0e91233b5119e104e06921e5d4a3a1c10c07361
ivanmeyer91 <37188867+ivanmeyer91@users.noreply.github.com>
2019-03-27 17:48:12 -0300

    arreglar conexion express con reactjs

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 import './login.css';
+//import request  from 'superagent';
 
 class Login extends Component {
+  
+//inicializo state
+constructor(props) {
+  super(props);
+  this.state = {}
+  this.connecToServer = this.connecToServer.bind(this)
+}
+
+  connecToServer() {
+    fetch('/api/frutas');
+  }
+//state = { frutas: [] };
+
+componentDidMount() {
+  this.connecToServer();
+}
+
+ // componentDidMount(){
+ //   this.getFrutas();
+ // }
+
+// getFrutas = () =>{
+//   fetch('/api/frutas')
+//   .then(res => res.text())
+//   .then(text => console.log(text));
+//   //.then(frutas => this.setState({ frutas }))
+// }
+
+
+
+
   render() {
+    //console.log(this.state.frutas);
     return (
       <div className="App">
         <header className="App-header">
           
           <h1>
-            Pantalla de login.js
+            Desde login.js
           </h1>
           <p>Para ir desde express cambie start:"nodemon src/server" desde el
-            Package.json e ir http://localhost:3000/Api 
+            Package.json e ir http://localhost:3000/api/frutas <br/>
+            Falta conectar el login con express que esta en la carpeta
+            server/index.js
+             
           </p>
           
         </header>
--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,33 @@
 //Configure el "start" del package.json para inizializar desde express
 
 //Declaro express para poder utilizarlo
-var express = require('express');
+var express = require("express");
 var app = express();
 //Declaro el puerto a usar
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Uso metodo USE en vez de GET porque funciona para todo
-app.use('/api', function (req, res) {
-  //Probando emojisense
-  var welcome = ('Bienvenido a EXPRESS 🖖')
+app.use(express.static('/api/frutas'));
+//Probando emojisense
+var welcome = "Bienvenido a EXPRESS 🖖";
 
-  //Tirando fruta
-  const Frutas = [ '🥝', '🍎', '🍊 ', welcome];
-  
-    res.json(Frutas);
-    
+//Tirando fruta
+const frutas = ["🥝", "🍎", "🍊 ", welcome];
+
+//res.json(Frutas);
+
+app.get("/api/frutas", (req, res) => {
+  res.json({
+    frutas: frutas
+  });
 });
 
-app.listen(3000, function () {
-  console.log('La app esta pegando en el puerto ' + port );
+app.get("/", (req, res) => {
+  res.json({
+    frutas: frutas
+  });
 });
- 
+
+app.listen(3000, function() {
+  console.log("La app esta pegando en el puerto " + port);
+});
